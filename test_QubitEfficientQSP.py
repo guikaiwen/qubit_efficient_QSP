2147de3fe4ac2b9cbe6bf61a3e19f17fcef076f2
Kevin Gui <kgui@uchicago.edu>
2024-08-10 17:38:01 -0500

    Revert "OOP updates"
    This reverts commit cf75d6e8a67e7e6a929488f9e9c46b173a2e786e.

--- test_QubitEfficientQSP.py
+++ test_QubitEfficientQSP.py
@@ -8,6 +8,10 @@ from braket.devices import LocalSimulator
 class TestQubitEfficientQSP(unittest.TestCase):
 
     def setUp(self):
+        self.complex_array = np.array([1/np.sqrt(2), 1j/np.sqrt(2)])
+        self.qsp = QubitEfficientQSP(self.complex_array)
+
+    def setUp(self):
             self.input_array = [0.22202689-0.27077295j, -0.39217787-0.33686943j, 0.10936691+0.15168349j,
                                 0.20360099+0.37102047j, -0.00122174-0.40592865j, -0.22520418+0.00997716j,
                                 -0.24708095+0.25582373j,  0.21319728+0.09208224j]
